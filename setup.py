b09853372cdc986675d063bc90db6312e6a7768c
koswu <ws00298046@163.com>
2021-01-08 10:56:00 +0800

    remove .rst readme

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
-#from distutils.core import setup, find_packages
+# from distutils.core import setup, find_packages
 from setuptools import setup, find_packages
 
 setup(name='justvpn'
-        ,version='0.4.0'
-        ,description=(
-            'An library which used for connect to JUST VPN'
-            )
-        ,long_description=open('README.rst').read()
-        ,author='Kos Wu'
-        ,author_email='ws00298046@163.com'
-        ,license='MIT License'
-        ,packages=["justvpn"]
-        ,platforms=["all"]
-        ,url='https://github.com/Koswu/JustVpn'
-        ,python_requires=">=2.6, !=3.0.*, !=3.2.*, !=3.3.*"
-        ,install_requires=["beautifulsoup4", "requests", "lxml"]
-        )
+      , version='0.4.0'
+      , description='An library which used for connect to JUST VPN'
+      , long_description=open('README.md').read()
+      , long_description_content_type="text/markdown"
+      , author='Kos Wu'
+      , author_email='ws00298046@163.com'
+      , license='MIT License'
+      , packages=["justvpn"]
+      , platforms=["all"]
+      , url='https://github.com/Koswu/JustVpn'
+      , python_requires=">=2.6, !=3.0.*, !=3.2.*, !=3.3.*"
+      , install_requires=["beautifulsoup4", "requests", "lxml"]
+      )
